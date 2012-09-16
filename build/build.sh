cd61b3b54f9224b92f86319b1178825ae249f106
Björn Schotte <bjoern.schotte@googlemail.com>
2012-09-16 14:21:11 +0200

    first version of build script

---
+++ build/build.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# pandoc binary, change to your needs
+PANDOC=pandoc
+
+# kindlegen binary, change to your needs
+KINDLEGEN=kindlegen
+
+# path to destination ebook dir in specific layout
+DEST=$1
+BOOKBASEFILE=$2
+
+# output directory and output files
+DEST_OUTPUT="$DEST/out"
+DEST_EPUB_COVERIMAGE="$DEST/cover/cover.jpg"
+DEST_OUTPUT_EPUB="$DEST_OUTPUT/$BOOKBASEFILE.epub"
+
+# chapter directory
+CHAPTER_DIR="$DEST/chapters"
+CHAPTERS=""
+
+# ePub metadata
+DEST_EPUB_METADATA="$DEST/epub/metadata.xml"
+
+# create ePub cover image
+PANDOC_EPUB_COVERIMAGE="--epub-cover-image=$DEST_EPUB_COVERIMAGE"
+
+# current working directory, will switch there later
+CUR=`pwd`
+
+echo "Building $DEST ..."
+
+# collecting chapters
+echo "Collecting chapter files ..."
+for i in `ls -1 $CHAPTER_DIR`
+do
+  echo $i
+  CHAPTER_FILE_FULLPATH="$CHAPTER_DIR/$i"
+  CHAPTERS="$CHAPTERS $CHAPTER_FILE_FULLPATH"
+done
+echo "found chapter files: $CHAPTERS"
+
+# generating epub format
+echo "Generating ePub ..."
+pandoc -f markdown -t epub --epub-metadata=$DEST_EPUB_METADATA $PANDOC_EPUB_COVERIMAGE -o $DEST_OUTPUT_EPUB $CHAPTERS
+
+# generating kindle
+echo "Generating .mobi ..."
+kindlegen $DEST_OUTPUT_EPUB
